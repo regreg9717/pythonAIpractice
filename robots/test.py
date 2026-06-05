6dfade881b9edd56410d3ff94997959e2782501a
刘春骁 <19293600872@163.com>
2026-06-05 20:02:48 +0800

    更新

--- robots/test.py
+++ robots/test.py
@@ -1,8 +1,22 @@
-import requests
+# import requests
+#
+# target_url = 'https://www.tiobe.com/tiobe-index/'
+#
+# response=requests.get(target_url)
+#
+# print(response.text)
 
-target_url = 'https://www.tiobe.com/tiobe-index/'
+from lxml import html
+with open("仙逆人物志.html","r",encoding="UTF-8") as f:
+    html_text=f.read()
+    document=html.fromstring(html_text)
 
-response=requests.get(target_url)
-
-print(response.text)
+    #解析表头
+    th_list=document.xpath("//table/thead/tr/th/text()")
+    print(th_list)
+    #获取所有行的数据
+    tr_list=document.xpath("//table/tbody/tr")
+    for tr in tr_list:
+        td_list=tr.xpath("./td/text()")
+        print(td_list)
 
