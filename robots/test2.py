d5b25416d10faf9f937b3f6abec5e16ede55ce53
刘春骁 <19293600872@163.com>
2026-06-06 15:47:52 +0800

    更新

--- robots/test2.py
+++ robots/test2.py
@@ -6,13 +6,13 @@ target_url = 'https://www.tiobe.com/tiobe-index/'
 
 response=requests.get(target_url)
 
-document=html.fromstring(response)
+document=html.fromstring(response.text)
 
 #解析数据
-th_list=document.xpath("//table/[@id='top20']/thead/tr/th/text()")
+th_list=document.xpath("//table[@id='top20']/thead/tr/th/text()")
 print(th_list)
 
-tr_list=document.xpath("//table/[@id='top20']/tbody/tr")
+tr_list=document.xpath("//table[@id='top20']/tbody/tr")
 for td in tr_list:
     td_list=td.xpath("./td/text()")
     print(td_list)
