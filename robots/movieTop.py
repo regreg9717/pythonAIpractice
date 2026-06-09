af67b4bb010b7e45c957064f3b66118ee7cf1508
刘春骁 <19293600872@163.com>
2026-06-09 20:23:23 +0800

    更新

--- robots/movieTop.py
+++ robots/movieTop.py
@@ -6,26 +6,36 @@ from  lxml import html
 TMDB_BASE_URL="https://www.themoviedb.org"
 TMDB_TOP_URL="https://www.themoviedb.org/movie/top-rated"
 
+#保存电影数据为csv文件
+def save_all_movies(all_movies):
+    pass
+
 #获取电影详情
-def get_movie_info(movie_info_url):
+def get_movie_info(movie_info_urls):
     pass
 
 def main():
-    #发送请求，获取高分电影榜单数据
+    #1.发送请求，获取高分电影榜单数据
     response=requests.get(TMDB_TOP_URL,timeout=60)
 
-    #解析数据，获取电影列表
+    #2.解析数据，获取电影列表
     document=html.fromstring(response.text)
     movie_list=document.xpath("//*[@id='page_1']/div[@class='card style_1']")
 
-    #遍历电影列表，获取电影详情
+    #3.遍历电影列表，获取电影详情
+    all_movies=[]
     for movie in movie_list:
         movie_urls =movie.xpath("./div/div/a/@href")
         if movie_urls:
             #电影详情的url
-            movie_info_urls=TMDB_BASE_URL+movie_urls[0]
+            movie_info_url=TMDB_BASE_URL+movie_urls[0]
+            #发送请求，获取电影的详情数据
+            movie_info=get_movie_info(movie_info_url)
+            all_movies.append(movie_info)
 
-    #保存数据，保存为csv文件
+
+    #4.保存数据，保存为csv文件
+    save_all_movies(all_movies)
 
 if __name__ =='__name__':
     main()
