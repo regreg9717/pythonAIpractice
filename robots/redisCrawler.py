feee2e2769266fcdd1918fbeb9c012fbd3f73d5f
刘春骁 <19293600872@163.com>
2026-06-11 21:20:03 +0800

    更新

--- robots/redisCrawler.py
+++ robots/redisCrawler.py
@@ -5,6 +5,7 @@ from lxml import html
 BASE_URL="https://doc.hnumi.com"
 BASE_URL_REDIS="https://doc.hnumi.com/bd/redis"
 
+#这是一个爬取云枢智库的爬虫代码
 def main():
     response=requests.get(BASE_URL,timeout=60)
     document=html.fromstring(response.text)
